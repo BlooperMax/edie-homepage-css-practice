57c4f49c5b15f0da91cdeab04780e16d442d5071
Edgardo <andrescarrasquel97@gmail.com>
2022-05-17 18:38:29 -0400

    proyect completed

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+const menuButton = document.querySelector('#menu');
+const closeButton = document.querySelector('#close');
+const modalMenu = document.querySelector('#modal');
+const mobileLinks = document.querySelectorAll('.links-mobile');
+
+const addClass = () => {
+    if (!modalMenu.classList.contains('none')) {
+        modalMenu.classList.add('none')
+    }
+}
+
+menuButton.addEventListener('click',()=>{
+    if (modalMenu.classList.contains('none')) {
+        modalMenu.classList.remove('none')
+    }
+});
+
+closeButton.addEventListener('click',addClass);
+
+mobileLinks.forEach((item)=>{
+    item.addEventListener('click',addClass)
+});
+
